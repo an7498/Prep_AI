f795a4b078d8f33b0fbe5e83314fee64a85de749
Anmol Sharan <165128023+an7498@users.noreply.github.com>
2024-12-12 15:14:25 +0530

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -3,9 +3,12 @@ import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 const isProtectedRoute = createRouteMatcher(["/dashboard(.*)", "/forum(.*)"]);
 
 export default clerkMiddleware((auth, req) => {
-  if (isProtectedRoute(req)) auth().protect();
+    if (isProtectedRoute(req)) auth().protect();
+}, {
+    publishableKey: process.env.CLERK_PUBLISHABLE_KEY
 });
 
 export const config = {
-  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+    matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 };
+
